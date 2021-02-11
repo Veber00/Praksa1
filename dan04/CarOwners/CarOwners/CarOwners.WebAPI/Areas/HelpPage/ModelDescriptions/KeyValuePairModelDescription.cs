d5f0949235461ded31ac691dd0f3054b652085c8
Hrvoje Veber <hrvoje.veber2000@gmail.com>
2021-02-11 16:40:23 +0100

    commit

--- dan04/CarOwners/CarOwners/CarOwners.WebAPI/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace CarOwners.WebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
--- dan04/CarOwners/CarOwners/CarOwners.WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace CarOwners.WebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
